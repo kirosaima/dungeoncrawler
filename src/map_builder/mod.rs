00f757dd07f65ebd315eb51fffcef86cb0f71d83
Mic Adams <kirosaima@protonmail.com>
2023-03-25 18:38:01 +0000

    added random themes

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,7 +1,19 @@
 use crate::prelude::*;
 mod automata;
+mod drunkard;
+mod empty;
+mod prefab;
 mod rooms;
+mod themes;
 use automata::CellularAutomataArchitect;
+use drunkard::DrunkardsWalkArchitect;
+use prefab::apply_prefab;
+use rooms::RoomsArchitect;
+use themes::*;
+
+pub trait MapTheme: Sync + Send {
+  fn tile_to_render(&self, tile_type: TileType) -> FontCharType;
+}
 
 trait MapArchitect {
   fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -14,12 +26,24 @@ pub struct MapBuilder {
   pub monster_spawns: Vec<Point>,
   pub player_start: Point,
   pub amulet_start: Point,
+  pub theme: Box<dyn MapTheme>,
 }
 
 impl MapBuilder {
   pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-    let mut architect = CellularAutomataArchitect {};
-    architect.new(rng)
+    let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+      0 => Box::new(DrunkardsWalkArchitect {}),
+      1 => Box::new(RoomsArchitect {}),
+      _ => Box::new(CellularAutomataArchitect {}),
+    };
+    let mut mb = architect.new(rng);
+    apply_prefab(&mut mb, rng);
+
+    mb.theme = match rng.range(0, 2) {
+      0 => DungeonTheme::new(),
+      _ => ForestTheme::new(),
+    };
+    mb
   }
 
   fn fill(&mut self, tile: TileType) {
